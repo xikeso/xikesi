b31fb19e2db17101e903a63b0b6c3599ac8a8928
lrh12546 <846522637@qq.com>
2019-06-29 14:16:02 +0800

    为红烧豆腐扣伙食费

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -3,7 +3,7 @@ package com.tedu.sp01.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//111111111111111111111122222222
+//111111111111111144444
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
